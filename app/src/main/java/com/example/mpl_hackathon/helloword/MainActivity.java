2742d5dcca4e09d222f821d572b614ebfca3c2da
max6me <mnaturel@itlink.fr>
2016-01-16 20:51:12 +0100

    On force l'écran à rester allumé

--- app/src/main/java/com/example/mpl_hackathon/helloword/MainActivity.java
+++ app/src/main/java/com/example/mpl_hackathon/helloword/MainActivity.java
@@ -17,6 +17,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -50,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate");
         setContentView(R.layout.content_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
@@ -75,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         unregisterReceiver(mBluetoothReceiver);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         stopService(new Intent(MainActivity.this, BluetoothLeService.class));
         super.onDestroy();
     }
